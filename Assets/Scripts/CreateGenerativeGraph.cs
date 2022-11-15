286bf0b3afd4f457ec6825571d3b0848670fa75c
Alex Gregory <984814@swansea.ac.uk>
2022-11-15 14:54:40 +0000

    Implemented Seed Generation
    Implemented a seed generation, with seeds varying between length 7 and length 18

---
+++ Assets/Scripts/CreateGenerativeGraph.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGenerativeGraph : MonoBehaviour
+{
+    public long seed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seed = Generate_Seed();
+        Debug.Log(seed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    static long Generate_Seed(){
+        // Max Rules of between 7 and 17 currently
+        System.Random rnd = new System.Random();
+        int seed_bottom = 1;
+        int seed_top = 10;
+        int seed_length = rnd.Next(7, 17);
+        Debug.Log(seed_length);
+
+        for(int i = 0; i < System.Math.Floor((double)(seed_length/2))-1; i++){
+            seed_bottom = seed_bottom * 10;
+            seed_top = seed_top * 10;
+        }
+        seed_top -= 1;
+
+        int seed_first = rnd.Next(seed_bottom, seed_top);
+        int seed_second = 0;
+        if (seed_length % 2 == 0){
+            seed_second = rnd.Next(seed_bottom, seed_top);
+        }
+        else{
+            seed_second = rnd.Next(seed_bottom*10, (seed_top+1)*10);
+        }
+
+        long seed = long.Parse(seed_first.ToString() + seed_second.ToString());
+
+        return seed;
+    }
+}
