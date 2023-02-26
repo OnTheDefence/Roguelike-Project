cf107619bbff067f8ae6d1c4f49bcf1e7c4cb398
Alex Gregory <984814@swansea.ac.uk>
2023-02-26 22:29:55 +0000

    Player Movement and Collision Complete

--- Assets/Scripts/CreateMapSprites.cs
+++ Assets/Scripts/CreateMapSprites.cs
@@ -12,8 +12,8 @@ public class CreateMapSprites : MonoBehaviour
         Vector3 scale_vector_down = new Vector3((float)0.75,(float)0.65,1);
         Vector3 box_vector_size = new Vector3(1, 1, 1);
 
-        GameObject sprites = new GameObject("Sprites");
-        sprites.transform.position = current_node.transform.position;
+        GameObject sprites = Instantiate (Resources.Load ("Prefab/Sprites") as GameObject);
+        sprites.transform.position = current_node.transform.position + new Vector3(0,0,1);
         sprites.transform.parent = current_node.transform;
 
 
@@ -32,13 +32,13 @@ public class CreateMapSprites : MonoBehaviour
             north_door_gate.AddComponent<SpriteRenderer>();
             north_door_gate.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Gates_north");
 
-            north_door.AddComponent<BoxCollider>();
-            north_door.GetComponent<BoxCollider>().size = box_vector_size;
-            north_door.GetComponent<BoxCollider>().center = new Vector3 ((float) 0,(float) 2.325, 1);
+            north_door.AddComponent<BoxCollider2D>();
+            north_door.GetComponent<BoxCollider2D>().size = box_vector_size;
+            north_door.GetComponent<BoxCollider2D>().offset = new Vector3 ((float) 0,(float) 2.325, 1);
 
-            north_door_gate.AddComponent<BoxCollider>();
-            north_door_gate.GetComponent<BoxCollider>().size = box_vector_size;
-            north_door_gate.GetComponent<BoxCollider>().center = new Vector3 ((float) 0,(float) 2.3, 1);
+            north_door_gate.AddComponent<BoxCollider2D>();
+            north_door_gate.GetComponent<BoxCollider2D>().size = box_vector_size;
+            north_door_gate.GetComponent<BoxCollider2D>().offset = new Vector3 ((float) 0,(float) 2.3, 1);
             
         } else{
             north_door.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Walls_north");            
@@ -60,13 +60,13 @@ public class CreateMapSprites : MonoBehaviour
             east_door_gate.AddComponent<SpriteRenderer>();
             east_door_gate.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Gates_east");
 
-            east_door.AddComponent<BoxCollider>();
-            east_door.GetComponent<BoxCollider>().size = box_vector_size;
-            east_door.GetComponent<BoxCollider>().center = new Vector3 ((float) 3.6,(float) 0, 1);
+            east_door.AddComponent<BoxCollider2D>();
+            east_door.GetComponent<BoxCollider2D>().size = box_vector_size;
+            east_door.GetComponent<BoxCollider2D>().offset = new Vector3 ((float) 3.6,(float) 0, 1);
 
-            east_door_gate.AddComponent<BoxCollider>();
-            east_door_gate.GetComponent<BoxCollider>().size = box_vector_size;
-            east_door_gate.GetComponent<BoxCollider>().center = new Vector3 ((float) 3.575,(float) 0, 1);
+            east_door_gate.AddComponent<BoxCollider2D>();
+            east_door_gate.GetComponent<BoxCollider2D>().size = box_vector_size;
+            east_door_gate.GetComponent<BoxCollider2D>().offset = new Vector3 ((float) 3.575,(float) 0, 1);
 
         } else{
             east_door.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Walls_east");
@@ -88,13 +88,13 @@ public class CreateMapSprites : MonoBehaviour
             south_door_gate.AddComponent<SpriteRenderer>();
             south_door_gate.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Gates_south");
 
-            south_door.AddComponent<BoxCollider>();
-            south_door.GetComponent<BoxCollider>().size = box_vector_size;
-            south_door.GetComponent<BoxCollider>().center = new Vector3 ((float) 0,(float) -2.325, 1);
+            south_door.AddComponent<BoxCollider2D>();
+            south_door.GetComponent<BoxCollider2D>().size = box_vector_size;
+            south_door.GetComponent<BoxCollider2D>().offset = new Vector3 ((float) 0,(float) -2.325, 1);
 
-            south_door_gate.AddComponent<BoxCollider>();
-            south_door_gate.GetComponent<BoxCollider>().size = box_vector_size;
-            south_door_gate.GetComponent<BoxCollider>().center = new Vector3 ((float) 0,(float) -2.3, 1);
+            south_door_gate.AddComponent<BoxCollider2D>();
+            south_door_gate.GetComponent<BoxCollider2D>().size = box_vector_size;
+            south_door_gate.GetComponent<BoxCollider2D>().offset = new Vector3 ((float) 0,(float) -2.3, 1);
 
         } else{
             south_door.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Walls_south");
@@ -116,27 +116,27 @@ public class CreateMapSprites : MonoBehaviour
             west_door_gate.AddComponent<SpriteRenderer>();
             west_door_gate.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Gates_west");
 
-            west_door.AddComponent<BoxCollider>();
-            west_door.GetComponent<BoxCollider>().size = box_vector_size;
-            west_door.GetComponent<BoxCollider>().center = new Vector3 ((float) -3.6,(float) 0, 1);
+            west_door.AddComponent<BoxCollider2D>();
+            west_door.GetComponent<BoxCollider2D>().size = box_vector_size;
+            west_door.GetComponent<BoxCollider2D>().offset = new Vector3 ((float) -3.6,(float) 0, 1);
 
-            west_door_gate.AddComponent<BoxCollider>();
-            west_door_gate.GetComponent<BoxCollider>().size = box_vector_size;
-            west_door_gate.GetComponent<BoxCollider>().center = new Vector3 ((float) -3.575,(float) 0, 1);
+            west_door_gate.AddComponent<BoxCollider2D>();
+            west_door_gate.GetComponent<BoxCollider2D>().size = box_vector_size;
+            west_door_gate.GetComponent<BoxCollider2D>().offset = new Vector3 ((float) -3.575,(float) 0, 1);
 
         } else{
             west_door.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Walls_west");
         }
 
 
-        GameObject map_skeleton = new GameObject("map_skeleton");
-        map_skeleton.transform.position = sprites.transform.position + new Vector3(0,0,1);
-        map_skeleton.transform.parent = sprites.transform;
-        map_skeleton.transform.localScale = scale_vector;
-        map_skeleton.AddComponent<SpriteRenderer>();
-        map_skeleton.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Skeleton Map");
-        map_skeleton.AddComponent<BoxCollider>();
-        map_skeleton.GetComponent<BoxCollider>().size = Vector3.Scale(map_skeleton.GetComponent<BoxCollider>().size, scale_vector_down);
+        // GameObject map_skeleton = new GameObject("map_skeleton");
+        // map_skeleton.transform.position = sprites.transform.position + new Vector3(0,0,1);
+        // map_skeleton.transform.parent = sprites.transform;
+        // map_skeleton.transform.localScale = scale_vector;
+        // map_skeleton.AddComponent<SpriteRenderer>();
+        // map_skeleton.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Skeleton Map");
+        // map_skeleton.AddComponent<BoxCollider2D>();
+        // map_skeleton.GetComponent<BoxCollider2D>().size = Vector3.Scale(map_skeleton.GetComponent<BoxCollider2D>().size, scale_vector_down);
 
         
     }
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D rb;
 
-    // Update is called once per frame
+    private Vector2 moveDirection;
+
     void Update()
     {
-        
+        // Get input for movement direction
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(horizontalInput, verticalInput).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // Move player using AddForce method
+        rb.AddForce(moveDirection * moveSpeed, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collide!");
+        rb.velocity = Vector2.zero;
     }
 }
